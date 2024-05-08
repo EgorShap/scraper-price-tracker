3dccbf3b42530fdb516546464deae2e4a604cc40
EgorShap <bear.egor10@gmail.com>
2024-05-08 09:26:33 +0300

    #1 created home screen, new components: search bar, navigation bar, carousel. Added icons and images to /public dir, created styles in 'global.css' and small changes in 'tailwind.config.ts'

---
+++ components/Navbar.tsx
@@ -0,0 +1,39 @@
+import { navIcons } from '@/icons'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <header className='w-full'>
+            <nav className='nav'>
+                <Link href='/' className='flex items-center gap-1'>
+                    <Image
+                        src='/assets/icons/logo.svg'
+                        width={27}
+                        height={27}
+                        alt='logo'
+                    />
+
+                    <p className='nav-logo'>
+                        Price<span className='text-primary'>Tracker</span>
+                    </p>
+                </Link>
+
+                <div className='flex items-center gap-5'>
+                    {navIcons.map((icon) => (
+                        <Image
+                            key={icon.alt}
+                            src={icon.src}
+                            alt={icon.alt}
+                            width={27}
+                            height={27}
+                        />
+                    ))}
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
